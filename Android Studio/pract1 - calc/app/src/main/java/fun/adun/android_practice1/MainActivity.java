b0c4e740bdd6929e29433ed3b4559f0a1c1e8c0c
AdunAdunov <evgenijvetrokrylov@gmail.com>
2024-11-18 22:33:24 +0500

    calc 1

---
+++ Android Studio/pract1 - calc/app/src/main/java/fun/adun/android_practice1/MainActivity.java
@@ -0,0 +1,90 @@
+package fun.adun.android_practice1;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText number1, number2;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Инициализация элементов
+        number1 = findViewById(R.id.number1);
+        number2 = findViewById(R.id.number2);
+        result = findViewById(R.id.result);
+
+        Button btnAdd = findViewById(R.id.btn_add);
+        Button btnSubtract = findViewById(R.id.btn_subtract);
+        Button btnMultiply = findViewById(R.id.btn_multiply);
+        Button btnDivide = findViewById(R.id.btn_divide);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate("+");
+            }
+        });
+
+        btnSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate("-");
+            }
+        });
+
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate("*");
+            }
+        });
+
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate("/");
+            }
+        });
+    }
+
+    private void calculate(String operation) {
+        try {
+            double num1 = Double.parseDouble(number1.getText().toString());
+            double num2 = Double.parseDouble(number2.getText().toString());
+            double res = 0;
+            switch (operation) {
+                case "+":
+                    res = num1 + num2;
+                    break;
+                case "-":
+                    res = num1 - num2;
+                    break;
+                case "*":
+                    res = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0) {
+                        res = num1 / num2;
+                    } else {
+                        result.setText("Error: Division by zero");
+                        return;
+                    }
+                    break;
+            }
+            result.setText("Result: " + res);
+        } catch (NumberFormatException e) {
+            result.setText("Error: Invalid input");
+        }
+    }
+}
